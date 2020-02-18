156758e16d2d30675a2cef642f643b1a91345e71
Aslı Ağılönü <MissAA@users.noreply.github.com>
2020-02-18 11:02:20 +0300

    deck model created
    The game rules have not been implemented yet

---
+++ src/game/PlayGame.java
@@ -0,0 +1,13 @@
+package game;
+
+import gameModel.Deck;
+
+public class PlayGame {
+
+	public static void main(String[] args) {
+		Deck deck = new Deck();
+		deck.shuffle();
+	
+	}
+
+}
--- src/gameModel/Deck.java
+++ src/gameModel/Deck.java
@@ -1,14 +1,42 @@
 package gameModel;
 
+import java.util.*;
+
+import com.sun.org.apache.bcel.internal.generic.ReturnaddressType;
+
 public class Deck {
 
+	private static ArrayList<Integer> deck = new ArrayList<Integer>();
+	
 	public Deck() {
-		// TODO Auto-generated constructor stub
+		for(int i = 1; i < 53; i++){
+			deck.add(i);
+		}
+	}
+	
+	public static void shuffle(){
+		Collections.shuffle(deck);	
+	}
+	
+	public static int drawCard()
+	{
+		if(!deck.isEmpty()){
+			int cardDrawn = deck.get(deckSize()-1);
+			deck.remove(deckSize()-1);
+			return cardDrawn;
+		}
+		else
+			//TODO fix the case for deck being empty
+			return 0;
+		
+		
+	}
+	
+	public static int deckSize()
+	{
+		return deck.size();
 	}
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-	}
 
 }
