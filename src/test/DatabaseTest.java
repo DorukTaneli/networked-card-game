cd4499bc69d82c171b73f9f4a8800c3f20f008b3
dtaneli16 <dtaneli16@ku.edu.tr>
2020-02-25 15:53:10 +0300

    add getGameInfo test

--- src/test/DatabaseTest.java
+++ src/test/DatabaseTest.java
@@ -1,5 +1,7 @@
 package test;
 
+import org.bson.Document;
+
 import database.MongoDB;
 
 public class DatabaseTest {
@@ -20,10 +22,15 @@ public class DatabaseTest {
 		System.out.println(s2);
 		db.printDatabaseContents();
 		
+		Document gameInfo = db.getGameInfo(1);
+		int rc = (int) gameInfo.get("RemainingCards");
+		System.out.println("Remaining Cards: " + rc);
+		int p1score = (int) gameInfo.get("Player1Score");
+		System.out.println("Player 1 score: " + p1score);
+		
 		db.deleteAll();
 		System.out.println("Deleted all game info");		
 		db.printDatabaseContents();
-		
 	}
 
 }
